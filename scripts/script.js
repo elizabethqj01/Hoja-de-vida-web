5a9e778c284b5cb1f1d3a5e6bb4e0727e9ba8471
elizabethqj <elizqui01@gmail.com>
2022-10-14 11:58:44 -0500

    Se agrega el proyecto

---
+++ scripts/script.js
@@ -0,0 +1,12 @@
+function onFacebook()
+        {
+            window.open("https://www.facebook.com/elizabeth.quinonezjimenez",''); 
+        }
+        function onTwiter()
+        {
+            window.open("https://twitter.com/elizqui01",''); 
+        }
+        function onInstagram()
+        {
+            window.open("https://www.instagram.com/elizabethqj/",''); 
+        }
